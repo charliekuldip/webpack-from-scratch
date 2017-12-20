0b980a2565b0c84d1bc77b94feaf08404678f45b
Charles Davis <charles.davis@borngroup.com>
2017-12-20 10:59:24 -0500

    First Commit

---
+++ index.js
@@ -0,0 +1,18 @@
+document.write("<h1>hello BILLLY!<br>S.S. Pecan!</h1>");
+
+const dress = () => {
+    document.write("<h1>MEMPHIS 3000!</h1>");
+}
+
+dress();
+
+
+class Car {
+    manufacturer(car) {
+        document.write(`I have a ${car}`);
+    }
+}
+
+const bmw = new Car;
+
+bmw.manufacturer('bmw');
